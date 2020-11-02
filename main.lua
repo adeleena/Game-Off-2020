fc6d4e8d614ccd27a5594a0a3c2992d103b01ad1
boctavian96 <boctavian96@yahoo.com>
2020-11-02 17:22:59 +0200

    update main an add colors constants.

--- main.lua
+++ main.lua
@@ -1,19 +1,35 @@
 
 Camera = require 'lib.hump.camera'
 Timer = require 'lib.hump.timer'
+Colors = require 'utils.colors'
 
 --UI should not be a global.
---UI = require 'lib.gspot'.
+UI = require 'lib.gspot'
 
 function love.load()
     --Load here scenes.
-    
+    oldLMBDown = false    
+    oldRMBDown = false
 end
 
 function love.update(dt)
 
+    oldLMBDown = love.mouse.isDown(1)
+    oldRMBDown = love.mouse.isDown(2)
 end
 
 function love.draw()
+
+end
+
+--Global keyboard
+function love.keypressed(key, scancode, isrepeat)
+    if key == 'q' then 
+        love.event.quit(1)
+    end
+end
+
+--Load an animation.
+function newAnimation(spreadsheet, width, height, duration)
 
 end
---
+++ utils/colors.lua
@@ -0,0 +1,12 @@
+local Colors = {
+    --RGBA
+    RED = {1, 0, 0, 1},
+    GREEN = {0, 1, 0, 1},
+    BLUE = {0, 0, 1, 1},
+    WHITE = {1, 1, 1, 1},
+    BLACK = {0, 0, 0, 1},
+    YELLOW = {1, 1, 0, 1},
+    PINK = {1, 0, 1, 1},
+}
+
+return Colors
