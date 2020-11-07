c649ff27a2922135e41a8106407747be3ce6f605
boctavian96 <boctavian96@yahoo.com>
2020-11-07 19:49:59 +0200

    Positioning the widgets

--- scene/mainmenu.lua
+++ scene/mainmenu.lua
@@ -1,29 +1,37 @@
 local mainmenu = {
     --Todo: @boctavian96 update this
     ui = require 'lib.gspot',
+    title = {},
 
     init = function(self)
         --Simple ui text...
 
+        local unit_size = self.ui.style.unit
+        local center = unit_size * 20
+
         --Simple ui buttons
-        local startButton = self.ui:button('Start', {x = 128, y = self.ui.style.unit, w = 128, h = self.ui.style.unit})
+        local startButton = self.ui:button('Start', {x = center, y = 15 * unit_size, w = 128, h = unit_size})
         startButton.click = function()
             --Debug
             print('[DEBUG]: I\'m clicking start')
         end
 
-        local aboutButton = self.ui:button('About', {x = 128, y = 3 * self.ui.style.unit, w = 128, h = self.ui.style.unit})
+        local aboutButton = self.ui:button('About', {x = center, y = 17 * unit_size, w = 128, h = unit_size})
         aboutButton.click = function()
             --Debug
             print('[DEBUG]: I\'m clicking about')
         end
 
-        local exitButton = self.ui:button('Exit', {x = 128, y = 5 * self.ui.style.unit, w = 128, h = self.ui.style.unit})
+        local exitButton = self.ui:button('Exit', {x = center, y = 19 * unit_size, w = 128, h = unit_size})
         exitButton.click = function()
             --Debug
             print('[DEBUG]: I\'m clicking exit')
             love.event.quit(1)
         end
+
+        table.insert(self.title, 'Main Menu')
+        table.insert(self.title, center)
+        table.insert(self.title, unit_size)
 
     end,
 
@@ -32,7 +40,8 @@ local mainmenu = {
     end,
 
     draw = function(self)
-        --self.drawDebug()
+        self:drawDebug()
+        love.graphics.print(self.title[1], self.title[2], self.title[3])
 
         self.ui:draw()
     end,
@@ -53,10 +62,8 @@ local mainmenu = {
         end
     end,
 
-    drawDebug = function() 
-        util.worldDebugDraw(20, 20)
-        love.graphics.setColor(Colors.RED)
-        love.graphics.print('Main Menu', 100, 100)
+    drawDebug = function(self) 
+        util.worldDebugDraw(self.ui.style.unit, self.ui.style.unit)
     end,
 
 }
