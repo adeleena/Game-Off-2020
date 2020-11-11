91102ebe210455113ae51dc0c247bf31f0a24242
Tavee <boctavian96@yahoo.com>
2020-11-11 18:23:00 +0200

    Updating some names.

--- gamestate.lua
+++ gamestate.lua
@@ -1,28 +1,28 @@
 local GameState = {
     states={},
-    state=1,
+    actual_state=1,
 
     update = function(self, dt) 
-        self.states[self.state]:update(dt)
+        self.states[self.actual_state]:update(dt)
         self:mouseP()
         self:mouseR()
         self:mouseW()
     end,
 
     draw = function(self)
-        self.states[self.state]:draw()
+        self.states[self.actual_state]:draw()
     end,
 
     keyboard = function(self, dt, key, scancode, isrepeat)
-        self.states[self.state]:handleKeyboard(dt, key, scancode, isrepeat)
+        self.states[self.actual_state]:handleKeyboard(dt, key, scancode, isrepeat)
     end,
 
     mouseP = function(self, x, y, button)
-        self.states[self.state]:mousePress(x, y, button)
+        self.states[self.actual_state]:mousePress(x, y, button)
     end,
 
     mouseR = function(self, x, y, button)
-        self.states[self.state]:mouseRelease(x, y, button)
+        self.states[self.actual_state]:mouseRelease(x, y, button)
     end,
 
     mouseW = function(self, x, y)
@@ -35,7 +35,7 @@ local GameState = {
 
     switchTo = function(self, state_id)
         --assert(stade_id <= #self.states, 'The state didn\'t exist')
-        self.state = state_id
+        self.actual_state = state_id
     end,
 
     switchToSName = function(self, state_name)
