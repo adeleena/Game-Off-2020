eaf249dc2d3c3d6a8b8bae08173ce86447cefdcf
Tavee <boctavian96@yahoo.com>
2020-11-11 19:51:40 +0200

    Skeleton for settings and planeshop.

---
+++ scene/settings.lua
@@ -0,0 +1,19 @@
+--Get the dependencies.
+local class = require 'lib.middleclass'
+local AbstractScene = require 'scene.abstract_scene'
+
+local settings = class('Settings', AbstractScene)
+
+function settings:init()
+
+end
+
+function settings:update(dt)
+
+end
+
+function settings:draw()
+
+end
+
+return settings
